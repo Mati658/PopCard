c9f7153588307a2ea5ded2fbc87795876b2b52da
Mati658 <matiasaltamira@outlook.com>
2025-01-23 21:20:19 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'PopCard',
+  webDir: 'www'
+};
+
+export default config;
